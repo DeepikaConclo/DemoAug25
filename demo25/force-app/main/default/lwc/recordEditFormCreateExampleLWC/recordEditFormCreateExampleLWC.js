d20bcc7bf65b06504a95c4853db56960c9e4cce0
hrithwikthathayil <hrithwik.s@conclotechnologies.com>
2022-08-25 10:36:45 +0530

    Commit-H2

--- demo25/force-app/main/default/lwc/recordEditFormCreateExampleLWC/recordEditFormCreateExampleLWC.js
+++ demo25/force-app/main/default/lwc/recordEditFormCreateExampleLWC/recordEditFormCreateExampleLWC.js
@@ -1,9 +1,30 @@
 import { LightningElement} from 'lwc';
+import { NavigationMixin } from 'lightning/navigation';
 export default class RecordEditFormCreateExampleLWC extends LightningElement {
+    connectedCallback() {
+        // Store the PageReference in a variable to use in handleClick.
+        // This is a plain Javascript object that conforms to the
+        // PageReference type by including 'type' and 'attributes' properties.
+        // The 'state' property is optional.
+        this.contactHomePageRef = {
+            type: 'standard__objectPage',
+            attributes: {
+                objectApiName: 'Contact',
+                actionName: 'home'
+            }
+        };
+        this[NavigationMixin.GenerateUrl](this.accountHomePageRef)
+            .then(url => this.url = url);
+    }
     handleSuccess(event) {
         console.log('onsuccess event recordEditForm',event.detail.id)
     }
     handleSubmit(event) {
+        event.preventDefault();
+        event.stopPropagation();
+        // Navigate to the Account Home page.
+        this[NavigationMixin.Navigate](this.contactHomePageRef);
         console.log('onsubmit event recordEditForm'+ event.detail.fields);
+
     }
 }
